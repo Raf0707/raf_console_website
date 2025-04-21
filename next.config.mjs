b6caf39573ad757e804ea90efbfe7a961d885667
Raf0707 <Raf_0707@mail.ru>
2025-04-21 17:03:07 +0300

    correct + update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // только если ты используешь `next export`
+    output: 'export', // 💥 важно для GitHub Pages
+    images: {
+        unoptimized: true, // отключает Next Image optimization (требует сервер)
+    },
+    trailingSlash: true, // делает ссылки вида /about/ (иначе будет /about)
+    basePath: "/raf_console_studio", // ⚠️ имя репозитория (без username)
 };
 
 export default nextConfig;
